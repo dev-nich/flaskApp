f2529d4f8746390dad6f76bf8a209988e26fe49b
dev-nich-m2 <csapuras@gmail.com>
2026-01-30 19:07:35 +0800

    feat: basic page

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title="Home", user=user)
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
