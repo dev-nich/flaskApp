40ba7ec322757603df8028203d8df675d7b22802
dev-nich-m2 <csapuras@gmail.com>
2026-02-09 11:53:29 +0800

    secure email req

--- config.py
+++ config.py
@@ -7,3 +7,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' +  os.path.join(basedir, 'app.db')
     POSTS_PER_PAGE=3
+    MAIL_SERVER=os.environ.get('MAIL_SERVER') or ''
+    MAIL_PORT=os.environ.get('MAIL_PORT') or ''
+    MAIL_USE_TLS=os.environ.get('MAIL_USE_TLS') or ''
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME') or ''
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD') or ''
+    MAIL_DEFAULT_SENDER=os.environ.get('MAIL_DEFAULT_SENDER') or ''
